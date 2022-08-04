318461b0ab64cb36a88e9dab8c3937662b2b2eb2
Jarax <jaraxkapila73@gmail.com>
2022-08-04 15:48:03 +0100

    add new tasks

--- mdn-react/src/App.js
+++ mdn-react/src/App.js
@@ -1,19 +1,30 @@
+import React, {useState} from "react";
+import {nanoid} from "nanoid";
 import Todo from "./components/Todo.js";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
-function App(props) {
-  console.log(props.tasks)
-  const taskList = props.tasks.map((task) => (
-  <Todo id={task.id} name={task.name} 
-  completed={task.completed} key={task.id} />));
+function App(props) { 
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+  const taskList = tasks.map((task) => (
+    <Todo id={task.id} 
+      name={task.name} 
+      completed={task.completed} 
+      key={task.id} />)
+    );
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        
+        <FilterButton />
         <button type="button" className="btn toggle-btn" aria-pressed="false">
           <span className="visually-hidden">Show </span>
           <span>Active</span>
--- mdn-react/src/components/Todo.js
+++ mdn-react/src/components/Todo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Todo(props){
-    console.log(props.name)
+    
     return (
         <li className="todo stack-small">
           <div className="c-cb">
